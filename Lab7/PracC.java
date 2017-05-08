663f135ecf11f9a3225c66bf22dce9f0deeb5460
Ravela Smyth <RavSmy@users.noreply.github.com>
2017-05-08 16:36:28 -0700

    Update PracC.java

--- Lab7/PracC.java
+++ Lab7/PracC.java
@@ -3,14 +3,17 @@ import java.util.*;
 
 public class PracC{
 
-	public static Scanner sc = new Scanner(System.in); 
-     
+	public static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args){
 		System.out.println(maximum(new int[]{10, 11, 12, 13, 12, 13, 1234}));
 		System.out.println(sum(new int[][]{{ 1,2,3,4},{ 1,2,2,1,1}}));
+		int even[]=(even(new int[]{1,5,2,4,7,8}));
+		for(int i : even)
+			System.out.println(i);
 	} //main
 
- 	
+
 	public static int maximum(int[] a){
 		int max = 0;
 		for(int i = 1 ; i < a.length; i++)
@@ -24,18 +27,16 @@ public class PracC{
 			for(int j = 0 ; j < a[i].length; j++)
 				sum += a[i][j];
 		return sum;
-	} 
+	}
 
 	public static int[] even(int[] a){
-		ArrayList evenlist = new ArrayList ();
-        j=0;
-        for (int i =0 ; i < a.length; i++)
-            if(a[i]%2 == 0){
-                evenlist.add(a[i]); j++;}    
-        
-        int even[]=evenlist.toArray(new int[j]);
-        return even;    
+		ArrayList<Integer> evenlist = new ArrayList<Integer>();
+		for (int i =0 ; i < a.length; i++)
+		    if(a[i]%2 == 0) evenlist.add(a[i]);
+			int even[] = new int[evenlist.size()];
+		for(int i =0 ; i < evenlist.size(); i++)
+			even[i] = evenlist.get(i);
+		return even;
 	}
-	
-}
 
+}
